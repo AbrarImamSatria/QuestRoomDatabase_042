baf2b2c675f5fe4eae67162bf330d71eedb3f2e5
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-14 16:54:00 +0700

    Menambahkan inisialisasi untuk DetailMhsViewModel dan UpdateMhsViewModel data dan validasi data

--- app/src/main/java/com/example/pertemuan10/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/PenyediaViewModel.kt
@@ -2,6 +2,7 @@ package com.example.pertemuan10.ui.viewmodel
 
 import android.text.Editable.Factory
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
@@ -13,6 +14,20 @@ object PenyediaViewModel {
         initializer {
             MahasiswaViewModel(
                 krsApp().containerApp.repositoryMhs
+            )
+        }
+
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
             )
         }
     }
