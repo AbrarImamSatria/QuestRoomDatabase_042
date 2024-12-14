c086ebc08dbd68e9778dde85d53e201d9e1f57c8
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-14 15:42:58 +0700

    Membuat data class HomeUiState

---
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan10.ui.viewmodel
+
+import com.example.pertemuan10.data.entity.Mahasiswa
+
+data class HomeUiState (
+    val listMhs: List<Mahasiswa> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
