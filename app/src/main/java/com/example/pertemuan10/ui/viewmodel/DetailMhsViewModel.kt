77167e46cb4dd9b8059a8344f9c818e291aa240a
abrarimamsatria09 <092004abrar@gmail.com>
2024-12-14 16:07:05 +0700

    Membuat data class DetailUiState

---
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/DetailMhsViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.pertemuan10.ui.viewmodel
+
+data class DetailUiState (
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+
+/*
+* Data class untuk menampung data yang akan ditampilkan di UI*/
+
